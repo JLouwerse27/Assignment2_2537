74b7ffb37d716b587d545cd8ab78edc02239e501
Joseph Louwerse <josephspam27@gmail.com>
2023-05-01 21:04:53 -0700

    Added contact page, BCrypt.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3005;//Childish Gambino
 
 const app = express();
+app.use(express.urlencoded({extended: false}));
+
+//Users and Passwords (in memory 'database')
+var users = []; 
 
 app.get("/", (req, res) => {
     res.send("<h1>Joseph's website</h1>");
@@ -11,7 +17,7 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
     var color = req.query.color;
 
-    res.send("<h1 style='color:" + color + ";'>Joseph Louwerse</h1>");
+    res.send("<h1 style='color:" + color + ";'>Joseph Louwerse is a cool guy!</h1>");
 });
 
 app.get('/meme/:id', (req, res) => {
@@ -34,19 +40,82 @@ app.get('/meme/:id', (req, res) => {
     }
 });
 
+app.get('/contact', (req,res) => {
+    var missingEmail = req.query.missing;
+    var html = `
+        email address:
+        <form action='/submitEmail' method='post'>
+            <input name='email' type='text' placeholder='email'>
+            <button>Submit</button>
+        </form>
+    `;
+    if (missingEmail) {
+        html += "<br> email is required";
+    }
+    res.send(html);
+});
+
+app.post('/submitEmail', (req,res) => {
+    var email = req.body.email;
+    if (!email) {
+        res.redirect('/contact?missing=1');
+    }
+    else {
+        res.send("Thanks for subscribing with your email: "+email);
+    }
+});
+
+app.get('/createUser', (req,res) => {
+    var html = `
+    <form action='/submitUser' method='post'>
+    <input name='username' type='text' placeholder='username'>
+    <input name='password' type='password' placeholder='password'>
+    <button>Submit</button>
+    </form>
+    `;
+    res.send(html);
+});
+
+app.post('/submitUser', (req,res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    var hashedPassword = bcrypt.hashSync(password, saltRounds);
+
+    users.push({ username: username, password: hashedPassword });
+
+    console.log(users);
+
+    var usershtml = "";
+    for (i = 0; i < users.length; i++) {
+        usershtml += "<li>" + users[i].username + ": " + users[i].password + "</li>";
+    }
+
+    var html = "<ul>" + usershtml + "</ul>";
+    res.send(html);
+});
+
 app.use(express.static(__dirname + "/public"));
 
 app.get("*", (req,res) => {
 	res.status(404);
-	res.send("<img src='/mike.avif' style='width:250px;'> <br/>"
+    var html = "<img src='/mike.avif' style='width:250px;'> <br/>"
     + "404 Error. <br/> Hey, pal. Looks like you've found yourself in a bit of a situation here. The page you're looking for? Yeah, it's not here. Not anymore, at least. These things happen, you know, like a half measure gone wrong."
 
     + "Now, I've seen my fair share of messes, but this one? It's not so bad. Just take a step back, gather yourself, and give it another shot. You might even find what you're looking for."
     
     + "In the meantime, do yourself a favor and click that little button down there. It'll take you back to safety - a place where things make sense. And remember, kid: No more half measures."
     
-    + "<br/> <button> Go Back Home </button>");
+    + " <form action='/redirectToHome' method='post'>"
+    + "<button>Go Back Home </button>"
+    + "</form>"
+	res.send(html);
 })
+
+app.post('/redirectToHome', (req,res) => {
+    res.redirect('/');
+});
+
 
 app.listen(port, () => {
     console.log("Node application listening on port " + port);
