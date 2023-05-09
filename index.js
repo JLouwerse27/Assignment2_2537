0ae1488284e301e496ec6043265c7e2e707d7ba2
Joseph Louwerse <josephspam27@gmail.com>
2023-05-08 21:34:17 -0700

    Added members.ejs, admin.ejs and bootstrap

--- index.js
+++ index.js
@@ -57,31 +57,13 @@ app.get("/", (req, res) => {
 
 app.get("/members", (req, res) => {
     var name = req.session.name;
-    var html;
+    var meme = Math.floor(Math.random() * 3 + 1); 
 
-    if (req.session.authenticated) {
-        html = `Hello, ` + name;
-        var meme = Math.floor(Math.random() * 3 + 1);
+    res.render("members", { 
+        auth: req.session.authenticated, 
+        name: name,
+        meme: meme });
 
-        if (meme == 1) {
-            html +=
-                "<br/> <img src='/breaking-bad-walter-white.gif' style='width:250px;'>";
-        } else if (meme == 2) {
-            html +=
-                "<br/> <img src='/i-love-undertime-slopper-undertime.gif' style='width:250px;'>";
-        } else if (meme == 3) {
-            html += "<br/> <img src='/corecore.webp' style='width:250px;'>";
-        }
-
-        html += "<form action='/logout' method='get'>"
-                    +"<button>Log out</button>"
-                +"</form>";
-
-    } else {
-        res.redirect("/");
-    }
-
-    res.send(html);
 });
 
 app.get("/logout", (req, res) => {
@@ -187,20 +169,16 @@ app.get("/loggedin", (req, res) => {
     res.render("loggedin");
 });
 
-app.use(express.static(__dirname + "/public"));
 
-app.get("*", (req, res) => {
-    res.status(404);
-    res.render("404");
-});
+
 
 app.post("/redirectToHome", (req, res) => {
     res.redirect("/");
 });
 
 app.get("/nosql-injection", async (req,res) => {
-	var email = req.query.email;
-
+    var email = req.query.email;
+    
 	if (!email) {
 		res.send(`<h3>no user provided - try /nosql-injection?user=name</h3> <h3>or /nosql-injection?user[$ne]=name</h3>`);
 		return;
@@ -216,50 +194,62 @@ app.get("/nosql-injection", async (req,res) => {
 	// and may result in revealing information about all users or a successful
 	// login without knowing the correct password.
 	if (validationResult.error != null) {  
-	   console.log(validationResult.error);
-	   res.send("<h1 style='color:darkred;'>A NoSQL injection attack was detected!!</h1>");
-	   return;
+        console.log(validationResult.error);
+        res.send("<h1 style='color:darkred;'>A NoSQL injection attack was detected!!</h1>");
+        return;
 	}	
-
+    
 	const result = await userCollection.find({email: email}).project({name: 1, email: 1, password: 1, _id: 1}).toArray();
-
+    
 	console.log(result);
-
+    
     res.send(`<h1>Hello ${email}</h1>`);
 });
 
 app.get("/about", (req, res) => {
     var color = req.query.color;
-
+    
     res.send(
         "<h1 style='color:" + color + ";'>Joseph Louwerse is a cool guy!</h1>"
-    );
-});
-
-app.get("/meme/:id", (req, res) => {
-    var meme = req.params.id;
-
-    if (meme == 1) {
-        res.send(
-            "Walt: <br/> <img src='/breaking-bad-walter-white.gif' style='width:250px;'>"
         );
-    } else if (meme == 2) {
-        res.send(
-            "Undertime Slopper: <br/> <img src='/i-love-undertime-slopper-undertime.gif' style='width:250px;'>"
-        );
+    });
+    
+    app.get("/meme"/*/:id"*/, (req, res) => {
+        // var meme = req.params.id;
+        var meme = 1;
+        
+        if (meme == 1) {
+            res.send(
+                "Walt: <br/> <img src='/breaking-bad-walter-white.gif' style='width:250px;'>"
+                );
+            } else if (meme == 2) {
+                res.send(
+                    "Undertime Slopper: <br/> <img src='/i-love-undertime-slopper-undertime.gif' style='width:250px;'>"
+                    );
     } else if (meme == 3) {
         res.send(
             "Undertime Slopper Again: <br/> <img src='/undertime-slopper-undertime.gif' style='width:250px;'>"
-        );
-    } else if (meme == 4) {
-        res.send(
-            "Corecore: <br/> <img src='/corecore.webp' style='width:250px;'>"
+            );
+        } else if (meme == 4) {
+            res.send(
+                "Corecore: <br/> <img src='/corecore.webp' style='width:250px;'>"
         );
     } else {
         res.send("Invalid meme id: " + meme);
     }
 });
 
+app.get("/admin", async (req, res) => {
+    const result = await userCollection.find().project({ name: 1, email:1, _id: 1 }).toArray();
+    res.render("admin", { users: result });
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("*", (req, res) => {
+    res.status(404);
+    res.render("404");
+});
 app.listen(port, () => {
     console.log("Node application listening on port " + port);
 });
